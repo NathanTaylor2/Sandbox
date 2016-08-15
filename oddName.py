a2b633a02391caf588a921be32f908927012b898
NathanTaylor2 <nathan.taylor2@my.jcu.edu.au>
2016-08-15 11:04:26 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,8 @@
 """
 Nathan Taylor
 """
+user_name = input("Please input your name.")
+second_letter = ""
+if user_name == "":
+    user_name = input("Please input your name")
+print(user_name[::2])
